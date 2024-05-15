cd10cfb229ada6c4c98287fd765f96864a28342a
FIRST_NAME <ribart412@gmail.com>
2024-05-15 14:20:06 +0300

    fix import statement

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,6 @@
 
 import React, { useState } from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Layout from '../components/Layout';
 
